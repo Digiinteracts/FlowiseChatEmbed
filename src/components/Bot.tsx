59c551f7c1c27c4d3083ce150db5cde391f3dce2
Digi Interact <contact@digiinteracts.com>
2024-06-25 15:33:07 +0530

    removed branding

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -1064,11 +1064,11 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
         </div>
       </div>
       <Badge
-            footer={props.footer}
-            badgeBackgroundColor={props.badgeBackgroundColor}
-            poweredByTextColor={props.poweredByTextColor}
-            botContainer={botContainer}
-          />
+        footer={props.footer}
+        badgeBackgroundColor={props.badgeBackgroundColor}
+        poweredByTextColor={props.poweredByTextColor}
+        botContainer={botContainer}
+      />
       {sourcePopupOpen() && <Popup isOpen={sourcePopupOpen()} value={sourcePopupSrc()} onClose={() => setSourcePopupOpen(false)} />}
     </>
   );
